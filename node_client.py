cd915fb5866f5d89ee7fe3b08fab6e3a0f1e25da
Hortofagos <120664745+Hortofagos@users.noreply.github.com>
2023-01-07 16:37:28 +0100

    Update node_client.py

--- node_client.py
+++ node_client.py
@@ -176,7 +176,7 @@ def node_protocol(rfb, rfb_response, transaction_pool, bill_pool):
                     active_udp_connections.append(addr[0])
                     itera = 0
                     while itera <= 1100:   
-                        time.sleep(0.5)
+                        time.sleep(0.1)
                         for nef in potential_conns_udp:
                             if nef[1] == addr[0].replace('::ffff:', ''):
                                 send('n' + ' '.join(nef))
