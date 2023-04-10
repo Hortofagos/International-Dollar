b4d7fd85378630c2f83ea17071f677017d9bb8dc
Hortofagos <120664745+Hortofagos@users.noreply.github.com>
2023-04-10 22:05:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ from print import full_bill, only_qr
 import sender_node
 import wallet_decryption
 import wallet_encryption
-import portforwardlib
 import platform
 
 try:
@@ -241,7 +240,6 @@ except Exception:
 
 #This function starts a network node
 def start():
-    result = portforwardlib.forwardPort(8888, 8888, None, None, "False", "TCP", 0, 'test', 'yes')
     with open('node_class.txt', 'w') as nc:
         nc.seek(0)
         nc.truncate()
